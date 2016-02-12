0a9053efdb550040d5760cf6d8b01ec3e563d6c0
Kevin Adams <kevinadams@kittymail.com>
2016-02-12 09:17:49 -0500

    Optimization

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -95,10 +95,10 @@ hook.Add( "HUDPaint", "HUDPaint_LawBox", function()
 
   local width, height = surface.GetTextSize( text )
 
-  draw.RoundedBox( 0, 1450, 20, 455, height + 35, Color( 0, 0, 0, 128 ) )
-  draw.DrawText( "Laws", "open_sans_25b", 1460, 25, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT )
+  draw.RoundedBox( 0, x * 0.63, y * 0.02, 455, height + 35, Color( 0, 0, 0, 128 ) )
+  draw.DrawText( "Laws", "open_sans_25b", x * 0.636, y * 0.02, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT )
 
-  draw.DrawNonParsedText(text, "open_sans_19b", 1460 + 1, 50 + 1, Color(0, 0, 0, 170), 0)
-  draw.DrawNonParsedText(text, "open_sans_19b", 1460 + 2, 50 + 2, Color(0, 0, 0, 100), 0)
-  draw.DrawNonParsedText(text, "open_sans_19b", 1460, 50, colour.white, 0)
+  draw.DrawNonParsedText(text, "open_sans_19b", x * 0.636, y * 0.05, Color(0, 0, 0, 170), 0)
+  draw.DrawNonParsedText(text, "open_sans_19b", x * 0.636, y * 0.05, Color(0, 0, 0, 100), 0)
+  draw.DrawNonParsedText(text, "open_sans_19b", x * 0.636, y * 0.05, colour.white, 0)
 end)
